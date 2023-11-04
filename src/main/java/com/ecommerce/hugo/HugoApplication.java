22d52e1a3915c432c33e5f7426545065164ecb37
Huzefa117 <huzefaelyas0052@gmail.com>
2023-11-04 20:53:36 +0530

    #pr self verification 2

--- src/main/java/com/ecommerce/hugo/HugoApplication.java
+++ src/main/java/com/ecommerce/hugo/HugoApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HugoApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(HugoApplication.class, args);
 	}
 
