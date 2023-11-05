3df4bfbcbc5504873054d44bf07caa4881f126e1
Huzefa117 <huzefaelyas0052@gmail.com>
2023-11-05 15:14:44 +0530

    CORS

--- src/main/java/com/ecommerce/hugo/HugoApplication.java
+++ src/main/java/com/ecommerce/hugo/HugoApplication.java
@@ -2,6 +2,9 @@ package com.ecommerce.hugo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class HugoApplication {
@@ -10,4 +13,17 @@ public class HugoApplication {
 		SpringApplication.run(HugoApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+								.allowedOrigins("http://localhost:4200")
+								.allowedMethods("GET", "POST", "PUT", "DELETE")
+								.allowedHeaders("Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization")
+								.maxAge(3600); // Optional: define max age for CORS preflight requests
+			}
+		};
+	}
 }
